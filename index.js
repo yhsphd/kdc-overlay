6db93efdf5319a041641ee7b9f1a649fd700ad3a
yhsphd <yhsphd05@gmail.com>
2024-04-11 01:25:26 +0900

    cors allow all for frontend dev case

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 const fs = require("fs");
 const path = require("path");
 
@@ -16,17 +20,17 @@ async function Init() {
     if (!configFileExists) {
       await fs.copyFileSync(
         path.join(__dirname + "/templates/config.default.js"),
-        path.join(process.cwd(), "./config.js")
+        path.join(process.cwd(), "./config.js"),
       );
       console.log("Default config file created! Please re-run the program after you complete!");
     }
     if (!streamConfigFileExists) {
       await fs.copyFileSync(
         path.join(__dirname + "/templates/streamConfig.default.js"),
-        path.join(process.cwd(), "./streamConfig.js")
+        path.join(process.cwd(), "./streamConfig.js"),
       );
       console.log(
-        "Default streamConfig file created! Please re-run the program after you complete!"
+        "Default streamConfig file created! Please re-run the program after you complete!",
       );
     }
     process.exit();
