6454e6a18c254a6867b1943f1618a4e566ed89ad
Yoon Hyungseok <yhsphd05@gmail.com>
2025-07-15 20:31:39 +0900

    CQ: Format code

--- src/entry.js
+++ src/entry.js
@@ -31,7 +31,9 @@ async function start(config) {
 
   // Run Server
   server.listen(config.port, () => {
-    logger.info(`osu!mania Long Note Tournament 4 overlay backend server running at http://${ip.address()}:${config.port}/`);
+    logger.info(
+      `osu!mania Long Note Tournament 4 overlay backend server running at http://${ip.address()}:${config.port}/`
+    );
   });
 }
 
